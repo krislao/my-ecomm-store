3381ca0c94bf0c21972912f4cad0f24764f9ca70
krislao <krislao13@gmail.com>
2021-02-21 19:11:01 +0800

    Remove link

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Space Jelly</a> store
+          Welcome to Space Jelly store
         </h1>
 
         <p className={styles.description}>
